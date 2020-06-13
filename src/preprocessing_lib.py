c1535aef93b3849ac5854491a9446f3f78300ab2
Mariana Lima <mariana.migliorini@engenharia.ufjf.br>
2020-06-13 18:02:44 -0300

    Add process function to preprocessing_lib

--- src/preprocessing_lib.py
+++ src/preprocessing_lib.py
@@ -53,4 +53,46 @@ def preprocessamento(file_path,porc = 30, fillna_param = 'zero'):
     else:
         filtered_df = 0
         print("Fill method wrong or not implemented yet")
-    return filtered_df    
+    return filtered_df   
+
+def process(df):
+   
+    """
+    Return a pandas matrix after some preprocessing
+    
+    Parameters
+    ----------
+    df : pandas dataframe
+             The Data Frame to be processed
+    
+    Returns
+    -------
+    df_processed : pandas dataframe
+        Pandas matrix contain all the lines but filtered some columns.
+    """
+    
+    df.drop(df.columns[df.isna().sum()/len(df) >= 0.3], axis=1, inplace=True)
+    
+    df['fl_rm'] = [True if i == 'SIM' else False for i in df['fl_rm']]
+    cols_obj2bool = df.select_dtypes(include=['object']).columns[df.select_dtypes(include=['object']).nunique() == 2]
+    df[cols_obj2bool] = df[cols_obj2bool].astype('bool')
+    
+    bool_threshold = 0.8
+    cols_bool2drop = []
+    for i in df.select_dtypes(include=['bool']).columns:
+        if (df[i].value_counts()/len(df) > bool_threshold).any():
+            cols_bool2drop.append(i)
+    df.drop(cols_bool2drop, axis=1, inplace=True)
+    
+    cols_toDrop = ['id','qt_socios_pf', 'qt_socios_pj', 'nm_meso_regiao', 'nm_micro_regiao', 'idade_empresa_anos',
+           'nm_divisao', 'nm_segmento', 'de_natureza_juridica', 'de_ramo','dt_situacao','de_saude_rescencia',
+           'qt_socios', 'vl_faturamento_estimado_aux', 'vl_faturamento_estimado_grupo_aux', 'fl_mei', 
+           'vl_total_veiculos_pesados_grupo', 'vl_total_veiculos_leves_grupo', 'nu_meses_rescencia', 'sg_uf_matriz']
+    df.drop(cols_toDrop, axis=1,inplace=True)
+    
+    df['setor'] = df['setor'].fillna(value='OUTRO')
+    col_fillna = df.columns[df.isna().sum() != 0]
+    df[col_fillna] = df[col_fillna].fillna(value='SEM INFORMACAO')
+    
+    df_processed = df
+    return df_processed
